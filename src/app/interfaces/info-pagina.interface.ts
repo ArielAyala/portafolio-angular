9e9cb845ccaa0402f05eb22b67e3bb5ffb01beb5
Ariel Ayala <arielayalamail@gmail.com>
2020-04-12 18:54:47 -0400

    Creado de interfaz para info de pagina

---
+++ src/app/interfaces/info-pagina.interface.ts
@@ -0,0 +1,13 @@
+
+
+export interface InfoPagina {
+    titulo?: string;
+    email?: string;
+    nomber_corto?: string;
+    pagina_autor?: string;
+    facebook?: string;
+    twitter?: string;
+    instagram?: string;
+    tumblr?: string;
+    equipo_trabajo?: any[];
+}
---
+++ src/app/services/info-pagina.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { InfoPagina } from '../interfaces/info-pagina.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InfoPaginaService {
+
+  info: InfoPagina = {};
+  cargada = false;
+
+  constructor(http: HttpClient) {
+
+    http.get('assets/data/data-pagina.json')
+      .subscribe((resp: InfoPagina) => {
+
+        this.cargada = true;
+        this.info = resp;
+        console.log(this.info);
+      })
+  }
+}
